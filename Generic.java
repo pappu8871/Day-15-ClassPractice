e4770d6a2fa55fa82bca6e5f5d402d0a994073ca
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-03 12:54:20 +0530

    Added:Welcome Generic

---
+++ Generic.java
@@ -0,0 +1,10 @@
+package com.classproblem;
+
+public class WelComePra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to practice Generic");
+	}
+
+}
