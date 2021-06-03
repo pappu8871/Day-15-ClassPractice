20d0220e314252a200e199c267aca48a6bfb50aa
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-03 13:03:18 +0530

    Added:PraGenericUC3.java

--- Generic.java
+++ Generic.java
@@ -1,27 +1,34 @@
+
 package com.classproblem;
 
-public class PraGenericUC2 <E> {
-	E[] myArray;
+public class PraGenericUC3 <X, Y, Z> {
+	X[] myXArray;
+	Y[] myYArray;
+	Z[] myZArray;
 
-	public PraGenericUC2(E[] myArray) {
-		this.myArray = myArray;
+
+	public PraGenericUC3(X[] myXArray, Y[] myYArray, Z[] myZArray) {
+		super();
+		this.myXArray = myXArray;
+		this.myYArray = myYArray;
+		this.myZArray = myZArray;
 	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integer[] a = {1,5,10};
 		Double[] b = {1.1, 3.5, 5.6};
 		Character[] c = {'a', 'c', 'g'};
 
-		toPrint(a);
-		toPrint(b);
-		toPrint(c);
-		new PraGenericUC2<Integer>(a).toPrint(a);
-		new PraGenericUC2<Double>(b).toPrint(b);
-		new PraGenericUC2<Character>(c).toPrint(c);
+//		toPrint(a);
+//		toPrint(b);
+//		toPrint(c);
+		new PraGenericUC3<Integer, Double, Character>(a, b, c).toPrint();
+
 	}
 	private void toPrint() {
-		toPrint(myArray);
+		toPrint(myXArray);
+		toPrint(myYArray);
+		toPrint(myZArray);
 	}
 	private static <E> void toPrint(E[] a) {
 
@@ -30,3 +37,4 @@ public class PraGenericUC2 <E> {
 		}
 	}
 }
+
