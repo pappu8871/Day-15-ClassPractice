981144a88ab742d7623f2d9f77db3cb53377b00a
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-03 12:56:47 +0530

    Added:PraGenericUC1.java

--- Generic.java
+++ Generic.java
@@ -1,10 +1,21 @@
 package com.classproblem;
 
-public class WelComePra {
+public class PraGenericUC1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to practice Generic");
-	}
+		Integer[] a = {1,5,10};
+		Double[] b = {1.1, 3.5, 5.6};
+		Character[] c = {'a', 'c', 'g'};
 
+		toPrint(a);
+		toPrint(b);
+		toPrint(c);
+	}
+	private static <E> void toPrint(E[] a) {
+
+		for (E i:a) {
+			System.out.println(i);  
+		}
+	}
 }
